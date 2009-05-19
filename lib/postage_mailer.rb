65acd65a3e477ab3a00999c26c93868bd608ce65
Jack Neto <jack@theworkinggroup.ca>
2009-05-19 16:45:16 -0400

    Simplified the email sending method

--- lib/postage_mailer.rb
+++ lib/postage_mailer.rb
@@ -1,40 +1,24 @@
 module PostageMailer
   
   def self.included(base)
-    base.send(:extend, ClassMethods)
     base.send(:include, InstanceMethods)
-    base.class_eval do
-      class << self
-        alias_method_chain :method_missing, :postage
-      end
-    end
   end
   
-  
-  # >> Class Methods -----------------------------------------------------
-  module ClassMethods
-    
-    def method_missing_with_postage(method_symbol, *parameters) 
-      if match = /^carry_([_a-z]\w*)/.match(method_symbol.to_s)
-        carry(new(match[1], *parameters))
-      else
-        method_missing_without_postage(method_symbol, *parameters)
-      end
-    end
+  # >> Instance Methods -----------------------------------------------------
+  module InstanceMethods
 
-
-    def carry(mail)
+    def perform_delivery_postage(mail)
       # Collect the headers
       header = {
-        'Subject'   => mail.subject, 
-        'From'      => mail.from
+        'Subject'   => self.subject, 
+        'From'      => self.from
       } 
-      mail.headers.each{ |k, v| header[k] = v }
+      self.headers.each{ |k, v| header[k] = v }
 
       # Collect the parts
       parts = {}
       attachments = {}
-      mail.parts.each do |part|
+      self.parts.each do |part|
         case part.content_disposition
           when 'inline' 
             parts[part.content_type] = part.body
@@ -44,22 +28,9 @@ module PostageMailer
       end
       parts[:attachments] = attachments unless attachments.blank?
 
-      # Collect specific postage data or the recipient list if no postage data was specified
-      postage_data = mail.postage_data.blank? ? mail.recipients : mail.postage_data
-
       # Send it all
-      Postage.new.send_message(parts, postage_data, {}, header)
+      Postage.new.send_message(parts, self.recipients, {}, header)
     end
 
-  end
-
-
-  # >> Instance Methods -----------------------------------------------------
-  module InstanceMethods
-    attr_writer :postage_data
-
-    def postage_data
-      @postage_data ||= { }
-    end
   end
 end
