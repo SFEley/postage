9d78f45fedb48de1c94355b354e1fa983d65125c
Jack Neto <jack@theworkinggroup.ca>
2009-06-11 12:43:16 -0400

    added recipient override

--- lib/postage_mailer.rb
+++ lib/postage_mailer.rb
@@ -1,5 +1,11 @@
 module PostageMailer
   
+  class Config
+    @@recipients ||= []
+    cattr_accessor :recipients
+  end
+  
+  
   def self.included(base)
     base.send(:include, InstanceMethods)
   end
@@ -10,38 +16,39 @@ module PostageMailer
     def perform_delivery_postage(mail)      
       require "base64"
 
+      
       # Collect the headers
-      header = {
-        'Subject'   => self.subject, 
-        'From'      => self.from
-      } 
-      self.headers.each{ |k, v| header[k] = v }
+      postage = {
+        :header => {
+          'Subject'   => self.subject, 
+          'From'      => self.from
+        }.merge(self.headers),
+        :parts => {},
+      }
 
       # Collect the parts
-      parts = {}
-      attachments = {}
-
       if self.parts.blank?
-        parts[self.content_type] = self.body
+        postage[:parts][self.content_type] = self.body
       else
         self.parts.each do |part|
           case part.content_disposition
             when 'inline'
-              parts[part.content_type] = part.body
+              postage[:parts][part.content_type] = part.body
             when 'attachment'
-              attachments[part.filename] = {
-                'filename' => part.filename,
-                'content_type' => part.content_type, 
-                'content' => Base64.encode64(part.body)
+              postage[:parts][:attachments] ||= {}
+              postage[:parts][:attachments][part.filename] = {
+                :content_type => part.content_type, 
+                :content      => Base64.encode64(part.body)
               }
           end
         end
       end
-
-      parts[:attachments] = attachments unless attachments.blank?
+      
+      # Check if we're overriding the recipients
+      self.recipients = PostageMailer::Config.recipients unless PostageMailer::Config.recipients.empty?
 
       # Send it all
-      Postage.new.send_message(parts, self.recipients, {}, header)
+      Postage.new.send_message(postage[:parts], self.recipients, {}, postage[:header])
     end
 
   end
