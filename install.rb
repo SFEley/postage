b8c29c6cd14d3650906126c5394ab3aad7f6e3ea
Oleg <oleg@theworkinggroup.ca>
2009-11-02 17:44:26 -0500

    fixing install.rb file

--- install.rb
+++ install.rb
@@ -1,13 +1,12 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
-require 'postage'
+require 'rubygems'
 
 puts "==========================================================================="
 puts "  Postage plugin successfully installed"
-puts "==========================================================================="
+puts "===========================================================================\n\n"
 puts "  Testing required Ruby Gems:"
 
 missing_gems = [ ]
-required_gems = %w[ httparty crack ]
+required_gems = %w[ httparty ]
 
 required_gems.each do |gem_name|
   found = false
@@ -17,32 +16,29 @@ required_gems.each do |gem_name|
   rescue Gem::LoadError
     missing_gems << gem_name
   end
-  
   puts "    #{gem_name} = #{found ? 'Installed' : 'Not found'}"
 end
 
 unless (missing_gems.empty?)
   puts ""
   puts "  The missing gems can usually be installed with:"
-  puts ""
   puts "     % sudo gem install #{missing_gems * ' '}"
 end
 
 puts ""
 puts "  Don't forget to add the gem dependencies to the environment.rb file:"
-puts ""
 required_gems.each do |gem_name|
   puts "    config.gem '#{gem_name}'"
 end
 
 puts ""
-puts "==========================================================================="
+puts "---------------------------------------------------------------------------"
 puts ""
 puts "  To finish configuring plugin please run:"
 puts ""
 puts "    rake postage:setup API_KEY=YOUR_API_KEY_HERE"
 puts "    rake postage:test"
 puts ""
-puts "==========================================================================="
-puts "  http://postageapp.com/"
-puts "==========================================================================="
+puts "---------------------------------------------------------------------------"
+puts ""
+puts "  For more information please visit http://postageapp.com/docs"
